f4b6e38f737b9998ecf7153e5ae8f9f75b64676c
willamesSantoos <willamesantos626@gmail.com>
2025-06-21 09:04:44 -0300

    feat: configuration h2

---
+++ backend/src/main/java/br/com/challenge_nexdom/backend/controllers/ProductController.java
@@ -0,0 +1,15 @@
+package br.com.challenge_nexdom.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @GetMapping
+    public String getProducts() {
+        return "products";
+    }
+}
