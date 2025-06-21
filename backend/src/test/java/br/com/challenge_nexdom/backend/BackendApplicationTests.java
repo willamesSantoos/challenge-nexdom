71a1a9ae377fad55ba01d5b009cd37f72ce5b102
willamesSantoos <willamesantos626@gmail.com>
2025-06-21 08:48:20 -0300

    feat: create project and configuration frontend

---
+++ backend/src/test/java/br/com/challenge_nexdom/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.challenge_nexdom.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
